0fa820a2ed71f81061e9081aea0a207dca0e67bf
matt <matiasberrueta@gmail.com>
2018-09-01 16:05:50 -0300

    Added 1st model organizations

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
 require 'rspec/rails'
+require 'factory_bot'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -11,6 +14,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  include FactoryBot::Syntax::Methods
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
