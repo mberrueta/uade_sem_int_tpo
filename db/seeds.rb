c868b3dc6608d5a5db9e295f31025757294c91aa
matt <matiasberrueta@gmail.com>
2018-10-07 16:37:12 -0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -143,17 +143,67 @@ Teacher.find_or_create_by(
 )
 
 program = Program.find_or_create_by(
-  id: "8ce7e4ae-606f-4a9f-ac06-d215c3cefb52",
+  id: "67e5f2be-af30-4f4d-b709-77b83656d0c7",
   subject: subject,
   day: 'Wed, 08 May 2019',
   done: false,
+  title: 'Inicio de la primer guerra mundial',
+  description: '',
+  picture_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrXY3vnXt16WGcCgDmhzVTHlZUKcQz_q6C2pcnuaEH64et74yI',
   class_number: 1
 )
 
+Program.find_or_create_by(
+  id: "ea2ba1c4-7d3a-4197-97dd-38b3c6d2452f",
+  subject: subject,
+  day: 'Wed, 12 May 2019',
+  done: false,
+  title: 'Fases de la primer guerra mundial',
+  description: '',
+  picture_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdmKhOXBRnXmULrhjDB7I-RNfQJapafmuYOJeybwZb9wLAaVvuIQ',
+  class_number: 2
+)
+
+Program.find_or_create_by(
+  id: "05102142-cc75-467f-9fbf-c35a8f891654",
+  subject: subject,
+  day: 'Wed, 15 May 2019',
+  done: false,
+  title: 'Fin de la primer guerra mundial',
+  description: '',
+  picture_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR-_WSg7RaReCbOo5zkiUrRzymuz6jSEXoUlGiqNo-IQ4P_8PloA',
+  class_number: 3
+)
+
+Program.find_or_create_by(
+  id: "81d5c87d-4bd8-458b-a319-08364ba3c001",
+  subject: subject,
+  day: 'Wed, 15 May 2019',
+  done: false,
+  title: 'Consecuencias de la primer guerra mundial',
+  description: '',
+  picture_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrAheM4tolDp2bCeTh-E_p94JWqKVG4aMvYYpFu1uXtvW4Ee88mg',
+  class_number: 3
+)
+
 Topic.find_or_create_by(
-  id: "f358567b-7f87-4839-a960-2dcef94554b0",
+  id: "59bb41e8-cea7-4ef6-bf82-c058e21b9524",
   program: program,
-  title: "Marcos Sanford II",
+  title: "Causas de la Primera Guerra Mundial",
+  description: nil
+)
+
+Topic.find_or_create_by(
+  id: "d7684532-f488-44cc-9b00-258f41b9cc3d",
+  program: program,
+  title: "El inicio de la gran guerra",
+  description: nil
+)
+
+Topic.find_or_create_by(
+  id: "1dd29f65-1e8a-4554-95fb-ca5430399641",
+  program: program,
+  title: "Desarrollo de la guerra",
   description: nil
 )
 
